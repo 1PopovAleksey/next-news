60ee49a87c24be0371bc1f99183e1f88eb672d4e
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-09 12:04:24 +0300

    feat: add news page

---
+++ pages/news.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+import styles from "./Header.module.scss";
+
+const News: NextPage = () => {
+  return (
+    <main>
+      <section>
+        <h1>News</h1>
+      </section>
+    </main>
+  );
+};
+
+export default News;
