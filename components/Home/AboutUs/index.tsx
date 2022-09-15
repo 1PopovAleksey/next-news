c148ae4cb880bbf3ac894163924d885a00e2f3ce
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-15 10:29:03 +0300

    feat: add map for AboutUs

--- components/Home/AboutUs/index.tsx
+++ components/Home/AboutUs/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { useId } from "react";
 
 import Title from "../../Title";
 import AboutUsCard from "../../Cards/AboutUsCard";
@@ -7,6 +8,23 @@ import man_news from "../../../public/assests/AboutUs/man-news.png";
 import man_calling from "../../../public/assests/AboutUs/man-calling.png";
 
 import styles from "./AboutUs.module.scss";
+
+const cards = [
+  {
+    image: man_news,
+    title: "Our story",
+    text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' uncover.",
+    link: "",
+  },
+  {
+    image: man_calling,
+    title: "Our story",
+    text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' uncover.",
+    link: "",
+  },
+];
+
+const id = useId;
 
 const AboutUs: NextPage = () => {
   return (
@@ -17,22 +35,15 @@ const AboutUs: NextPage = () => {
             <Title title="About us" />
           </div>
           <div className={styles.cards}>
-            <AboutUsCard
-              image={man_news}
-              title={"Our story"}
-              text={
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' uncover."
-              }
-              link={""}
-            />
-            <AboutUsCard
-              image={man_calling}
-              title={"Our story"}
-              text={
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' uncover."
-              }
-              link={""}
-            />
+            {cards.map((card, index) => (
+              <AboutUsCard
+                key={`${id}-${index}`}
+                image={card.image}
+                title={card.title}
+                text={card.text}
+                link={card.link}
+              />
+            ))}
           </div>
         </div>
       </div>
